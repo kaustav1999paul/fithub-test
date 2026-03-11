b8f2e5cda3a15ed9314d51dd87ee3ae29237825d
Kaustav Paul <kaustavpaul347@gmail.com>
2026-03-11 23:28:45 +0530

    Initial commit

---
+++ src/main/java/com/example/fithub_test/FithubTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.fithub_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FithubTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fithub_test/FithubTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.fithub_test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FithubTestApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
